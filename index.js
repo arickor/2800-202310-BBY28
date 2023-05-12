0f2b803bab067149da1d5a09c1d31160a7b7ac5a
Aric Or <aricor10945@gmail.com>
2023-05-12 10:45:49 -0700

    Updated createProfile and saveProfile pages

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Dependencies
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-const MongoStore = require('connect-mongo');
-require('dotenv').config();
-const url = require('url');
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const MongoStore = require("connect-mongo");
+require("dotenv").config();
+const url = require("url");
 
 const app = express();
 
 const Joi = require("joi");
-const { database } = require('./dbconnection');
+const { database } = require("./dbconnection");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -29,78 +29,84 @@ const userCollection = database.db(mongodb_database).collection("users");
 const expireTime = 60 * 60 * 1000; // 1 hour in milliseconds
 const saltRounds = 10;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
-})
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
+  crypto: {
+    secret: mongodb_session_secret,
+  },
+});
 
-app.use(session({
+app.use(
+  session({
     secret: node_session_secret,
     store: mongoStore,
     saveUninitialized: false,
-    resave: true
-}    
-));
+    resave: true,
+  })
+);
 
-function sessionAuth(req,res,next){
-    if(req.session.authenticated){
-        next();
-    }else{
-        res.redirect("/login");
-    }
+function sessionAuth(req, res, next) {
+  if (req.session.authenticated) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
 }
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: req.session.username,
+    email: req.session.email,
+  });
 });
 
-app.get("/login",(req,res)=>{
-    res.send(`
+app.get("/login", (req, res) => {
+  res.send(`
     <form action="/loggingin" method="POST">
     <input type="email" name="email" placeholder="email">
     <input type="password" name="password" placeholder="password">
     <button type="submit">Login</button>
     </form>`);
-}
-);
-
-app.post("/loggingin",async (req,res)=>{
-    var email = req.body.email;
-    var password = req.body.password;
-    
-    const schema = Joi.string().email().required();
-    const validation = schema.validate(email);
-    if (validation.error != null) {
-        console.log(validation.error);
-        res.redirect('/login');
-        return;
-    }
-    const result = await userCollection.find({email: email}).project({email: 1, password: 1, username: 1, _id: 1}).toArray();
-    console.log(result);
-    if (result.length == 0) {
-        console.log("email not found");
-        res.redirect('/login');
-        return;
-    }
-    if (await bcrypt.compareSync(password, result[0].password)) {
-        console.log("password match");
-        req.session.authenticated = true;
-        req.session.email = email;
-        req.session.username = result[0].username;
-        req.session.cookie.maxAge = expireTime;
-        res.redirect('/');
-    } else {
-        console.log("password mismatch");
-        res.redirect('/login');
-    }
 });
 
-app.get("/signup",(req,res)=>{
-    res.send(`
+app.post("/loggingin", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  const schema = Joi.string().email().required();
+  const validation = schema.validate(email);
+  if (validation.error != null) {
+    console.log(validation.error);
+    res.redirect("/login");
+    return;
+  }
+  const result = await userCollection
+    .find({ email: email })
+    .project({ email: 1, password: 1, username: 1, _id: 1 })
+    .toArray();
+  console.log(result);
+  if (result.length == 0) {
+    console.log("email not found");
+    res.redirect("/login");
+    return;
+  }
+  if (await bcrypt.compareSync(password, result[0].password)) {
+    console.log("password match");
+    req.session.authenticated = true;
+    req.session.email = email;
+    req.session.username = result[0].username;
+    req.session.cookie.maxAge = expireTime;
+    res.redirect("/");
+  } else {
+    console.log("password mismatch");
+    res.redirect("/login");
+  }
+});
+
+app.get("/signup", (req, res) => {
+  res.send(`
     <form action="/signingup" method="POST">
     <input type="text" name="username" placeholder="username">
     <input type="email" name="email" placeholder="email">
@@ -109,57 +115,67 @@ app.get("/signup",(req,res)=>{
     </form>`);
 });
 
-app.post("/signingup",async (req,res)=>{
-    var username = req.body.username;
-    var password = req.body.password;
-    var email = req.body.email;
-    if (!username || !password || !email){
-        res.send(`Please enter all fields
+app.post("/signingup", async (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var email = req.body.email;
+  if (!username || !password || !email) {
+    res.send(`Please enter all fields
         <a href="/signup">Go back</a>`);
-    }else{
-        const schema = Joi.object({
-            username: Joi.string().max(30).required(),
-            email: Joi.string().email().required(),
-            password: Joi.string().max(30).required()
-        });
-        const validation = schema.validate({username, email, password});
-        if (validation.error != null) {
-            console.log(validation.error);
-            res.redirect('/signup');
-            return;
-        }
-        var user = await userCollection.findOne({username: username});
-        if(user){
-            res.send(`User already exists
-            <a href="/signup">Go back</a>`);
-        }else{
-            var encryptedPassword = await bcrypt.hash(password,saltRounds);
-            var newUser = {
-                username: username,
-                password: encryptedPassword,
-                email: email
-            }
-            await userCollection.insertOne(newUser);
-            req.session.authenticated = true;
-            req.session.email = email;
-            req.session.username = username;
-            req.session.cookie.maxAge = expireTime;
-            res.redirect("/createprofile");
-        }
+  } else {
+    const schema = Joi.object({
+      username: Joi.string().max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().max(30).required(),
+    });
+    const validation = schema.validate({ username, email, password });
+    if (validation.error != null) {
+      console.log(validation.error);
+      res.redirect("/signup");
+      return;
     }
+    var user = await userCollection.findOne({ username: username });
+    if (user) {
+      res.send(`User already exists
+            <a href="/signup">Go back</a>`);
+    } else {
+      var encryptedPassword = await bcrypt.hash(password, saltRounds);
+      var newUser = {
+        username: username,
+        password: encryptedPassword,
+        email: email,
+      };
+      await userCollection.insertOne(newUser);
+      req.session.authenticated = true;
+      req.session.email = email;
+      req.session.username = username;
+      req.session.cookie.maxAge = expireTime;
+      res.redirect("/createprofile");
+    }
+  }
 });
 
-app.get("/createprofile",sessionAuth,(req,res)=>{
-    res.send("createprofile");
+app.post("/createprofile", sessionAuth, (req, res) => {
+  res.render("createprofile", {
+    username: req.session.username,
+    email: req.session.email,
+  });
+});
+
+app.post("/saveProfile", sessionAuth, (req, res) => {
+  res.render("saveProfile", {
+    username: req.session.username,
+    email: req.session.email,
+  });
 });
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req,res) => {
-	res.status(404);
-	res.send('404');
-})
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("404");
+});
 
 app.listen(port, () => {
-    console.log("Server running on port: " + port);
+  console.log("Server running on port: " + port);
 });
